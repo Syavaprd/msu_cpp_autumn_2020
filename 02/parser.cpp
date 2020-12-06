daf03887ec7f31d7dfac382c49e3ddff6714ccdc
Syavaprd <38497601+Syavaprd@users.noreply.github.com>
2020-12-07 00:52:06 +0300

    Changes

---
+++ 02/parser.cpp
@@ -0,0 +1,50 @@
+#include "parser.h"
+
+onint inttoken = nullptr;
+ontoken strtoken = nullptr;
+edge start = nullptr;
+edge finish = nullptr;
+
+void intcallback(onint callback) {
+    inttoken = callback;
+}
+
+void strcallback(ontoken callback) {
+    strtoken = callback;
+}
+
+void startcallback(edge callback) {
+    start = callback;
+}
+
+void finishcallback(edge callback) {
+    finish = callback;
+}
+
+void parse(const std::string& str) {
+    if (start != nullptr) start();
+    size_t pos_1 = 0;
+    size_t found;
+    std::string token;
+    bool flag = true;
+    while ((found = str.find_first_of(" \n\t", pos_1)) != std::string::npos) {
+        token = str.substr(pos_1, found - pos_1);
+        if ((isdigit(token[0])) && (inttoken != nullptr)) {
+            int number = std::stoi(token.c_str());
+            inttoken(number);
+        }
+        else if (strtoken != nullptr) strtoken(token);
+        pos_1 = str.find_first_not_of(" \n\t", found);
+    }
+    if (pos_1 == std::string::npos) {
+        if (finish != nullptr) finish();
+        return;
+    }
+    token = str.substr(pos_1);
+    if ((isdigit(token[0])) && (inttoken != nullptr)) {
+        int number = std::stoi(token.c_str());
+        inttoken(number);
+    }
+    else if (strtoken != nullptr) strtoken(token);
+    if (finish != nullptr) finish();
+}
---
+++ 02/parser.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstddef>
+
+using ontoken = void (*)(std::string& token);
+using edge = void (*)();
+using onint = void (*)(int token);
+
+void intcallback(onint callback);
+
+void strcallback(ontoken callback);
+
+void startcallback(edge callback);
+
+void finishcallback(edge callback);
+
+void parse(const std::string& str);
